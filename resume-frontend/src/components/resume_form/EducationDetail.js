0fc33860cae02310b9fae1593ee2565866b96952
rohitsamal.mvteams <rohitsamal.mvteams@gmail.com>
2023-01-18 09:52:36 +0530

    solved error : input field not updating on state change and loose focus just after the first letter

--- resume-frontend/src/components/resume_form/EducationDetail.js
+++ resume-frontend/src/components/resume_form/EducationDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import EducationForm from "./EducationForm";
 const EducationDetail = () => {
   const [names, setNames] = useState([]);
   useEffect(() => {
@@ -32,28 +33,28 @@ const EducationDetail = () => {
     console.log(count);
   };
 
-  const removeEducation = (index) => {
-    console.log(count.length);
-    if (count.length > 1) {
-      educationDetails.splice(index, 1);
-      let newCount = count.filter((data) => {
-        return data !== count[index];
-      });
-      console.log(newCount);
-      setCount((prevState) => newCount);
-    }
-    if (count.length === 1) {
-      setEducationDetail((prevState) => [
-        {
-          "college/uni": "",
-          passing_year: "",
-          marks: "",
-          graduation: false,
-          post_graduation: false,
-        },
-      ]);
-    }
-  };
+  // const removeEducation = (index) => {
+  //   console.log(count.length);
+  //   if (count.length > 1) {
+  //     educationDetails.splice(index, 1);
+  //     let newCount = count.filter((data) => {
+  //       return data !== count[index];
+  //     });
+  //     console.log(newCount);
+  //     setCount((prevState) => newCount);
+  //   }
+  //   if (count.length === 1) {
+  //     setEducationDetail((prevState) => [
+  //       {
+  //         "college/uni": "",
+  //         passing_year: "",
+  //         marks: "",
+  //         graduation: false,
+  //         post_graduation: false,
+  //       },
+  //     ]);
+  //   }
+  // };
   const handleChange = (e, name, index1) => {
     console.log(name);
     setEducationDetail((prevState) => {
@@ -88,149 +89,161 @@ const EducationDetail = () => {
         <form className="education-form bg-white " onSubmit={addEducation}>
           <div className="education-form-div">
             {count.map((data, index) => (
-              <React.Fragment key={index + Math.random()}>
-                <h3 className="margin-0 margin-bottom-5">
-                  Education {index + 1}
-                </h3>
-                <input
-                key="college"
-                  className="input-field margin-bottom-5"
-                  placeholder="Select Your College..."
-                  type="text"
-                  list="college"
-                  name="college/uni"
-                  value={educationDetails[index]["college/uni"]}
-                  onChange={(e) => {
-                    setEducationDetail((prevState) => {
-                      const newState = prevState.map((obj, index1) => {
-                        if (index === index1) {
-                          return { ...obj, "college/uni": e.target.value };
-                        }
-                        return obj;
-                      });
-                      console.log(newState);
-                      return newState;
-                    });
-                  }}
-                />
-                <datalist id="college" name="college">
-                  <optgroup>
-                    <option value=""></option>
-                    {names.map((data, index) => (
-                      <option
-                        key={index + Math.random()}
-                        value={data.college_name}
-                      >
-                        {data.college_name}
-                      </option>
-                    ))}
-                  </optgroup>
-                </datalist>
+              // <React.Fragment key={index + Math.random()}>
+              //   <h3 className="margin-0 margin-bottom-5">
+              //     Education {index + 1}
+              //   </h3>
+              //   <input
+              //   key="college"
+              //     className="input-field margin-bottom-5"
+              //     placeholder="Select Your College..."
+              //     type="text"
+              //     list="college"
+              //     name="college/uni"
+              //     value={educationDetails[index]["college/uni"]}
+              //     onChange={(e) => {
+              //       setEducationDetail((prevState) => {
+              //         const newState = prevState.map((obj, index1) => {
+              //           if (index === index1) {
+              //             return { ...obj, "college/uni": e.target.value };
+              //           }
+              //           return obj;
+              //         });
+              //         console.log(newState);
+              //         return newState;
+              //       });
+              //     }}
+              //   />
+              //   <datalist id="college" name="college">
+              //     <optgroup>
+              //       <option value=""></option>
+              //       {names.map((data, index) => (
+              //         <option
+              //           key={index + Math.random()}
+              //           value={data.college_name}
+              //         >
+              //           {data.college_name}
+              //         </option>
+              //       ))}
+              //     </optgroup>
+              //   </datalist>
 
-                <input
-                  className="input-field margin-bottom-5"
-                  type="text"
-                  name="passing_year"
-                  id="passing_year"
-                  placeholder="Enter your passing year"
-                  value={
-                    educationDetails[index] !== undefined
-                      ? educationDetails[index]["passing_year"]
-                      : " "
-                  }
-                  onChange={(e) => {
-                    handleChange(e, e.target.name, index);
-                  }}
-                />
+              //   <input
+              //     className="input-field margin-bottom-5"
+              //     type="text"
+              //     name="passing_year"
+              //     id="passing_year"
+              //     placeholder="Enter your passing year"
+              //     value={
+              //       educationDetails[index] !== undefined
+              //         ? educationDetails[index]["passing_year"]
+              //         : " "
+              //     }
+              //     onChange={(e) => {
+              //       handleChange(e, e.target.name, index);
+              //     }}
+              //   />
 
-                <input
-                  className="input-field margin-bottom-5"
-                  type="text"
-                  name="marks"
-                  id="marks"
-                  value={
-                    educationDetails[index] !== undefined
-                      ? educationDetails[index]["marks"]
-                      : ""
-                  }
-                  placeholder="Enter Passing Marks"
-                  onChange={(e) => {
-                    handleChange(e, e.target.name, index);
-                  }}
-                />
+              //   <input
+              //     className="input-field margin-bottom-5"
+              //     type="text"
+              //     name="marks"
+              //     id="marks"
+              //     value={
+              //       educationDetails[index] !== undefined
+              //         ? educationDetails[index]["marks"]
+              //         : ""
+              //     }
+              //     placeholder="Enter Passing Marks"
+              //     onChange={(e) => {
+              //       handleChange(e, e.target.name, index);
+              //     }}
+              //   />
 
-                <fieldset
-                  className="personal-detail-fieldset margin-bottom-5"
-                  onChange={(e) => {
-                    console.log(e.target.value);
-                    if (e.target.value === "graduation") {
-                      setEducationDetail((prevState) => {
-                        const newState = prevState.map((obj, index1) => {
-                          if (
-                            e.target.value === "graduation" &&
-                            index1 === index
-                          ) {
-                            return {
-                              ...obj,
-                              graduation: true,
-                              post_graduation: false,
-                            };
-                          }
-                          return obj;
-                        });
-                        console.log(newState);
-                        return newState;
-                      });
-                    } else {
-                      setEducationDetail((prevState) => {
-                        const newState = prevState.map((obj, index1) => {
-                          if (
-                            e.target.value === "post_graduation" &&
-                            index1 === index
-                          ) {
-                            return {
-                              ...obj,
-                              graduation: false,
-                              post_graduation: true,
-                            };
-                          }
-                          return obj;
-                        });
-                        console.log(newState);
-                        return newState;
-                      });
-                    }
-                  }}
-                >
-                  <legend>Type</legend>
-                  <input
-                    type="radio"
-                    name={"education_type" + index}
-                    value="graduation"
-                    id="graduation"
-                    checked={educationDetails[index]["graduation"]}
-                    onChange={() => {}}
-                  />
-                  <label htmlFor="graduation">Graduation</label>
-                  <input
-                    type="radio"
-                    name={"education_type" + index}
-                    value="post_graduation"
-                    id="post_graduation"
-                    checked={educationDetails[index]["post_graduation"]}
-                    onChange={() => {}}
-                  />
-                  <label htmlFor="post_graduation">Post Graduation</label>
-                </fieldset>
+              //   <fieldset
+              //     className="personal-detail-fieldset margin-bottom-5"
+              //     onChange={(e) => {
+              //       console.log(e.target.value);
+              //       if (e.target.value === "graduation") {
+              //         setEducationDetail((prevState) => {
+              //           const newState = prevState.map((obj, index1) => {
+              //             if (
+              //               e.target.value === "graduation" &&
+              //               index1 === index
+              //             ) {
+              //               return {
+              //                 ...obj,
+              //                 graduation: true,
+              //                 post_graduation: false,
+              //               };
+              //             }
+              //             return obj;
+              //           });
+              //           console.log(newState);
+              //           return newState;
+              //         });
+              //       } else {
+              //         setEducationDetail((prevState) => {
+              //           const newState = prevState.map((obj, index1) => {
+              //             if (
+              //               e.target.value === "post_graduation" &&
+              //               index1 === index
+              //             ) {
+              //               return {
+              //                 ...obj,
+              //                 graduation: false,
+              //                 post_graduation: true,
+              //               };
+              //             }
+              //             return obj;
+              //           });
+              //           console.log(newState);
+              //           return newState;
+              //         });
+              //       }
+              //     }}
+              //   >
+              //     <legend>Type</legend>
+              //     <input
+              //       type="radio"
+              //       name={"education_type" + index}
+              //       value="graduation"
+              //       id="graduation"
+              //       checked={educationDetails[index]["graduation"]}
+              //       onChange={() => {}}
+              //     />
+              //     <label htmlFor="graduation">Graduation</label>
+              //     <input
+              //       type="radio"
+              //       name={"education_type" + index}
+              //       value="post_graduation"
+              //       id="post_graduation"
+              //       checked={educationDetails[index]["post_graduation"]}
+              //       onChange={() => {}}
+              //     />
+              //     <label htmlFor="post_graduation">Post Graduation</label>
+              //   </fieldset>
 
-                <button
-                  className="input-field bg-lt-purple margin-bottom-5"
-                  type="button"
-                  onClick={() => removeEducation(index)}
-                >
-                  REMOVE EDUCATION
-                </button>
-              </React.Fragment>
+              //   <button
+              //     className="input-field bg-lt-purple margin-bottom-5"
+              //     type="button"
+              //     onClick={() => removeEducation(index)}
+              //   >
+              //     REMOVE EDUCATION
+              //   </button>
+              // </React.Fragment>
+              <EducationForm
+                key={index}
+                educationDetails={educationDetails}
+                setEducationDetail={setEducationDetail}
+                addEducation={addEducation}
+                count={count}
+                names={names}
+                addCount={addCount}
+                setCount={setCount}
+                // removeEducation={removeEducation}
+                index={index}
+              />
             ))}
             <div
               className="margin-bottom-5"
--- resume-frontend/src/components/resume_form/EducationForm.js
+++ resume-frontend/src/components/resume_form/EducationForm.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 const EducationForm = (props) => {
-    
-  const handleChange = (e, name, index1) => {
-    // props.setEducationDetail((prevState) => ({
-    //   ...prevState[index],
-    //   [name]: e.target.value,
-    // }));
+  const [collegeRef, setCollegeRef] = useState({
+    "college/uni": "",
+    passing_year: "",
+    marks: "",
+    graduation: false,
+    post_graduation: false,
+  });
+  const handleChange = (e, name, index) => {
+    setCollegeRef((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+  const handleBlur = (e, name, index1) => {
     console.log(name);
     props.setEducationDetail((prevState) => {
-      const newState = prevState.map((obj) => {
-        if (obj[name] === "") {
-          return { ...obj, [name]: e.target.value };
+      const newState = prevState.map((obj, index) => {
+        if (index === index1) {
+          return { ...obj, [name]: collegeRef[name] };
         }
         return obj;
       });
@@ -18,6 +23,38 @@ const EducationForm = (props) => {
       return newState;
     });
   };
+  const removeEducation = (index) => {
+    console.log(props.count.length);
+    if (props.count.length > 1) {
+      props.educationDetails.splice(index, 1);
+      let newCount = props.count.filter((data) => {
+        return data !== props.count[index];
+      });
+      console.log(newCount);
+      props.setCount((prevState) => newCount);
+    }
+    if (props.count.length === 1) {
+      props.setEducationDetail((prevState) => [
+        {
+          "college/uni": "",
+          passing_year: "",
+          marks: "",
+          graduation: false,
+          post_graduation: false,
+        },
+      ]);
+      setCollegeRef((prevState) => ({
+        "college/uni": "",
+        passing_year: "",
+        marks: "",
+        graduation: false,
+        post_graduation: false,
+      }));
+    }
+  };
+  useEffect(() => {
+    console.log(props.educationDetails);
+  }, []);
   return (
     <>
       <h3 className="margin-0 margin-bottom-5">Education {props.index + 1}</h3>
@@ -27,7 +64,9 @@ const EducationForm = (props) => {
         type="text"
         list="college"
         name="college/uni"
-        value={props.educationDetails[props.index]["college/uni"]}
+        value={collegeRef["college/uni"]}
+        // value={props.educationDetails[props.index]["college/uni"]}
+        onBlur={(e) => handleBlur(e, e.target.name, props.index)}
         onChange={(e) => {
           handleChange(e, e.target.name, props.index);
         }}
@@ -49,11 +88,9 @@ const EducationForm = (props) => {
         name="passing_year"
         id="passing_year"
         placeholder="Enter your passing year"
-        value={
-          props.educationDetails[props.index] !== undefined
-            ? props.educationDetails[props.index]["passing_year"]
-            : " "
-        }
+        value={collegeRef["passing_year"]}
+        // value={props.educationDetails[props.index]["college/uni"]}
+        onBlur={(e) => handleBlur(e, e.target.name, props.index)}
         onChange={(e) => {
           handleChange(e, e.target.name, props.index);
         }}
@@ -64,12 +101,10 @@ const EducationForm = (props) => {
         type="text"
         name="marks"
         id="marks"
-        value={
-          props.educationDetails[props.index] !== undefined
-            ? props.educationDetails[props.index]["marks"]
-            : ""
-        }
         placeholder="Enter Passing Marks"
+        value={collegeRef["marks"]}
+        // value={props.educationDetails[props.index]["college/uni"]}
+        onBlur={(e) => handleBlur(e, e.target.name, props.index)}
         onChange={(e) => {
           handleChange(e, e.target.name, props.index);
         }}
@@ -78,7 +113,7 @@ const EducationForm = (props) => {
       <fieldset
         className="personal-detail-fieldset margin-bottom-5"
         onChange={(e) => {
-            console.log(e.target.value)
+          console.log(e.target.value);
           if (e.target.value === "graduation") {
             props.setEducationDetail((prevState) => {
               const newState = prevState.map((obj, index) => {
@@ -93,8 +128,11 @@ const EducationForm = (props) => {
           } else {
             props.setEducationDetail((prevState) => {
               const newState = prevState.map((obj, index) => {
-                if (e.target.value === "post_graduation" && index === props.index) {
-                    return { ...obj, graduation: false, post_graduation: true };
+                if (
+                  e.target.value === "post_graduation" &&
+                  index === props.index
+                ) {
+                  return { ...obj, graduation: false, post_graduation: true };
                 }
                 return obj;
               });
@@ -128,7 +166,7 @@ const EducationForm = (props) => {
       <button
         className="input-field bg-lt-purple margin-bottom-5"
         type="button"
-        onClick={() => props.removeEducation(props.index)}
+        onClick={() => removeEducation(props.index)}
       >
         REMOVE EDUCATION
       </button>
